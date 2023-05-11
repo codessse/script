d78e6a871b488c35e525f542fabbd474f7828e3d
codessse <57928832+codessse@users.noreply.github.com>
2023-05-11 19:13:42 +0800

    Update docker_postgresql_upgrade.sh

--- docker_postgresql_upgrade.sh
+++ docker_postgresql_upgrade.sh
@@ -7,25 +7,36 @@ NEW='15'
 DBname='testDB'
 SQLFILE='./testDB_backup.sql'
 
-
-docker pull "postgres:$OLD"
+echo "Create Old PostgreSQL container..."
 docker run -dit --name postgres-upgrade-testing -e POSTGRES_PASSWORD=password -v "$PWD/$OLD/data":/var/lib/postgresql/data "postgres:$OLD"
 
-docker exec -i postgres-upgrade-testing psql -U postgres -c "CREATE DATABASE $DBname"
+sleep 1
+
+echo "Container Created"
+echo "Processing Old PostgreSQL..."
+docker exec -i postgres-upgrade-testing psql -U postgres -c "CREATE DATABASE $DBname;"
 cat $SQLFILE | docker exec -i postgres-upgrade-testing psql -U postgres -d $DBname
 
 docker stop postgres-upgrade-testing
 docker rm postgres-upgrade-testing
 
+echo "Upgrading Database..."
 docker run --rm -v "$PWD":/var/lib/postgresql "tianon/postgres-upgrade:$OLD-to-$NEW" --link
 
-docker pull "postgres:$NEW"
+echo "Processing New PostgreSQL..."
 docker run -dit --name postgres-upgrade-testing -e POSTGRES_PASSWORD=password -v "$PWD/$NEW/data":/var/lib/postgresql/data "postgres:$NEW"
 
 sudo chown -R $USER:$USER ..
+echo "Exporting New PostgreSQL file..."
 docker exec -i postgres-upgrade-testing pg_dump -U postgres $DBname > ../${DBname}_dump_$(date +%Y-%m-%d_%H_%M_%S).sql
 
 cd ..
+
+echo "Done"
+
+docker stop postgres-upgrade-testing
+docker rm postgres-upgrade-testing
 sudo rm -fr postgres-upgrade-testing
 
 unset OLD NEW DBname SQLFILE
+echo "Container Deleted"
